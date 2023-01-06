57163fe6d3f2449bb4fd272a190fc80d60221be4
Leif Gruendler <mistake91@web.de>
2023-01-06 15:09:53 +0100

    2cards hardcoded

--- componets/Card/index.js
+++ componets/Card/index.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import styled from "styled-components";
+
 
 export default function Card() {
   return (
+    <styledDiv>
     <ul>
       <li>
         <h2>Orange Red and Green Abstract Painting</h2>
@@ -20,5 +23,52 @@ export default function Card() {
         <p>Steve Johnson</p>
       </li>
     </ul>
+
+<ul>
+<li>
+  <h2>Blue and Red</h2>
+</li>
+<li>
+  <Image
+    src={
+      "https://example-apis.vercel.app/assets/art/blue-and-red.jpg"
+    }
+    alt=""
+    height={300}
+    width={300}
+  ></Image>
+</li>
+<li>
+  <p>Jung-Hua Lui</p>
+</li>
+</ul>
+
+<ul>
+      <li>
+        <h2>Clay Bust Sculptures</h2>
+      </li>
+      <li>
+        <Image
+          src={
+            "https://example-apis.vercel.app/assets/art/clay-bust-sculptures.jpg"
+          }
+          alt=""
+          height={300}
+          width={300}
+        ></Image>
+      </li>
+      <li>
+        <p>dilara irem</p>
+      </li>
+    </ul>
+    </styledDiv>
   );
 }
+
+const styledDiv = styled.div `
+width: 100vw;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+`
--- componets/Container.styled.js
+++ componets/Container.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
+  width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
